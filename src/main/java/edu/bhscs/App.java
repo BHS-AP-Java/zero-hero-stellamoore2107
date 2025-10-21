2a6de3e3a5cfa8745b5a722950bfaeef4ddfd68b
stellamoore2107 <2024031@apps.nsd.org>
2025-10-21 16:26:47 +0000

    I have made an app file.

---
+++ src/main/java/edu/bhscs/App.java
@@ -0,0 +1,12 @@
+package edu.bhscs;
+
+public class App {
+  public static void main(String args[]){
+    date_10_21_25();
+    System.out.println("I am the app.");
+  }
+
+  public static void date_10_21_25(){
+    System.out.println("Today's work");
+  }
+}
