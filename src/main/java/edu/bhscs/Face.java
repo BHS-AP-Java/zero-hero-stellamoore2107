7843df1198daadcdcf50b8d6f716c672bfd2746c
stellamoore2107 <2024031@apps.nsd.org>
2025-11-25 18:04:01 +0000

    Something about interfaces

---
+++ src/main/java/edu/bhscs/Face.java
@@ -0,0 +1,22 @@
+package edu.bhscs;
+
+// Draws a simple face
+public class Face implements Offsetable {
+  void draw(){
+    this.draw(null);
+  }
+
+  void draw(Offsetable other){
+    System.out.println(" ^ ^ ");
+    System.out.println("  |  ");
+    System.out.println("\\___/");
+  }
+
+  public int getWidth(){
+    return 5;
+  }
+
+  public int getOffset(Offsetable below) {
+    return 0;
+  }
+}
---
+++ src/main/java/edu/bhscs/Offsetable.java
@@ -0,0 +1,9 @@
+package edu.bhscs;
+
+public interface Offsetable {
+  int getWidth();
+
+  int getOffset();
+
+  void draw();
+}
